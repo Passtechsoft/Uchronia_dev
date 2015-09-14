8424d84d1f00f2c2f1af0c4224568c67ca4749b0
{Alexis Gros;creeperdeking;tewos} <alexis.gros99@gmail.com>
2015-09-14 22:06:44 +0200

    Commit gameplay+organisation

---
+++ Organization/Hierarchie.mm
@@ -0,0 +1,194 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1442255879292" ID="ID_1175218483" MODIFIED="1442258420593" STYLE="fork" TEXT="Coordinateur general">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Il connait la ligne directrice du projet et est en dialogue constant avec toutes les parties du d&#233;vellopement pour assurer la coh&#233;rence et la finalit&#233; du projet
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1442255935353" ID="ID_474147420" MODIFIED="1442258420594" POSITION="right" STYLE="fork" TEXT="Directeur game design">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Il occupe un role central dans le d&#233;vellopement du jeu: c'est lui qui imagine le gameplay et l'apparence du jeu, avant d'organiser l'&#233;quipe capable de concr&#233;tiser sa vision du jeu.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1442256084245" ID="ID_1829101423" MODIFIED="1442258420594" STYLE="fork" TEXT="Directeur creatif">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1442256539615" ID="ID_196772281" MODIFIED="1442258420595" STYLE="fork" TEXT="Map designer">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442256579381" ID="ID_209463574" MODIFIED="1442258420595" STYLE="fork" TEXT="Modeliste">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257370305" ID="ID_181944379" MODIFIED="1442258420596" STYLE="fork" TEXT="Testeur Gameplay">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442256641615" ID="ID_1945829131" MODIFIED="1442258420596" STYLE="fork" TEXT="Realisateur cinematique">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1442256096701" ID="ID_1425088309" MODIFIED="1442258420596" STYLE="fork" TEXT="Directeur artistique">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1442256587089" ID="ID_1808912848" MODIFIED="1442258420597" STYLE="fork" TEXT="Graphiste">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257703380" ID="ID_133581597" MODIFIED="1442258420597" STYLE="fork" TEXT="Layout designer">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1442256112001" ID="ID_1378651987" MODIFIED="1442258420598" STYLE="fork" TEXT="Chef programmation">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Souvent lui m&#234;me programmeur &#224; temps plein, le chef de programmation est celui qui fice les r&#232;gles et tranche les probl&#232;mes de code, on raconte qu'il serait le plus souvent barbu.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1442256375521" ID="ID_1499319513" MODIFIED="1442258420598" STYLE="fork" TEXT="Programmeur jeu video">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257520477" ID="ID_1827699421" MODIFIED="1442258420599" STYLE="fork" TEXT="Programmeur web">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257528038" ID="ID_1478243096" MODIFIED="1442258420600" STYLE="fork" TEXT="Programmeur outils">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1442256396995" ID="ID_1346830499" MODIFIED="1442258420601" STYLE="fork" TEXT="Directeur audio">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Il imagine la musique avec l'orchestrateur, puis la compose. Il est garant de l'int&#233;grit&#233; de l'ambiance sonore et musicale du jeu.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1442256410605" ID="ID_1877978052" MODIFIED="1442258420601" STYLE="fork" TEXT="Orchestrateur">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442256417346" ID="ID_701429031" MODIFIED="1442258420602" STYLE="fork" TEXT="Musicien">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442256444715" ID="ID_1248623190" MODIFIED="1442258420602" STYLE="fork" TEXT="Ing&#xe9;nieur son">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442256450535" ID="ID_1248729054" MODIFIED="1442258420603" STYLE="fork" TEXT="Recordeur">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257638735" ID="ID_937257040" MODIFIED="1442258420603" STYLE="fork" TEXT="Doubleur">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442256485637" ID="ID_38969378" MODIFIED="1442258420604" STYLE="fork" TEXT="Mixeur">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1442256011974" ID="ID_1392095642" MODIFIED="1442258420604" POSITION="right" STYLE="fork" TEXT="Producteur executif">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Il a droit de vie et de mort sur le projet, parce que c'est lui qui a le pognon!
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442256268173" ID="ID_1849854824" MODIFIED="1442258420605" POSITION="right" STYLE="fork" TEXT="Sc&#xe9;nariste general">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Il a cr&#233;&#233; l'univers, les personnages principaux, le jeu, c'est son univers, et il devra se montrer cr&#233;atif et r&#233;aliste dans sa vision des personnages et des h&#233;ros.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1442256303447" ID="ID_1810650875" MODIFIED="1442258420606" STYLE="fork" TEXT="Assistant sc&#xe9;nariste">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      L'assistant sc&#233;nariste enrichit l'univers en s'occupant de tisser des intrigues secondaires et de donner plus de profondeur &#224; l'univers en s'occupant des d&#233;tails, ce que le sc&#233;nariste general n'a pas toujours le temps de faire
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257574990" ID="ID_521430789" MODIFIED="1442258420606" STYLE="fork" TEXT="Traducteur">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1442257292856" ID="ID_1239246452" MODIFIED="1442258420606" POSITION="left" STYLE="fork" TEXT="Manager marketing">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257300042" ID="ID_1906557961" MODIFIED="1442258420607" POSITION="left" STYLE="fork" TEXT="Betatesteur">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257345930" ID="ID_968824521" MODIFIED="1442258420607" POSITION="left" STYLE="fork" TEXT="Testeur gameplay">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257429727" ID="ID_1494450264" MODIFIED="1442258420608" POSITION="left" STYLE="fork" TEXT="Comptabilit&#xe9;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257444903" ID="ID_1232755064" MODIFIED="1442258420608" POSITION="left" STYLE="fork" TEXT="Ing&#xe9;nieur r&#xe9;seau et materiel">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257456922" ID="ID_1321594653" MODIFIED="1442258420608" POSITION="left" STYLE="fork" TEXT="L&#xe9;giste">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257463446" ID="ID_110886873" MODIFIED="1442258420609" POSITION="left" STYLE="fork" TEXT="Community manager">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257488267" ID="ID_1260642304" MODIFIED="1442258420609" POSITION="left" STYLE="fork" TEXT="Support technique">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1442257772156" ID="ID_1909618506" MODIFIED="1442258420610" POSITION="right" STYLE="fork" TEXT="Expert scientifique">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      L'expert scientifique est l&#224; pour donner son avis ou mener des invertigation pour donner des pr&#233;cisions dans un domaine particulier ayant rapport avecle jeu. Il peut &#234;tre inexistant ou indispensable en fonction du type de jeu (et de sc&#233;nario) imagin&#233;.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</map>
